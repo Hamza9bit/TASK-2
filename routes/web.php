cb80fb1a2008d5f85e975c60b796f25c512a10f9
Hamza <59793243+Hamza9bit@users.noreply.github.com>
2022-08-01 18:59:18 +0500

    bug change

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('management','App\Http\Controllers\Management');
+Route::resource('management','App\Http\Controllers\ManagementController');
