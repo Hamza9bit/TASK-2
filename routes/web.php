2fb92cef25b28edceaa3e25d1d04aa8872c66b04
Hamza <59793243+Hamza9bit@users.noreply.github.com>
2022-08-03 10:44:59 +0500

    newer changes

--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,6 @@ Route::get('/dashboard', function() {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-//require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
 
 Route::resource('management','App\Http\Controllers\ManagementController');
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
